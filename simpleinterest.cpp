fb6ddf487013345e43d867288b19429f8c746f8e
anshul goyal <anshulgoyal211@gmail.com>
2021-06-26 15:57:48 +0530

    Simple Interest

---
+++ simpleinterest.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int p,t,r;
+    float si;
+    cin>>p>>t>>r;
+    si=p*r*t/100.0;
+    
+    cout<<"Simple Interest="<<si<<endl;
+
+    return 0; 
+}
