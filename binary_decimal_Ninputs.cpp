c0a27b840520e0589747b2023441c6b363889f7b
anshul goyal <anshulgoyal211@gmail.com>
2021-07-03 00:47:10 +0530

    binary to decimal with n inputs

---
+++ binary_decimal_Ninputs.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int no;
+    int N;
+
+    cin >> N;
+    while(N>0){
+        cin >> no;
+
+        int ans = 0;
+        int p = 1;
+
+        while (no > 0)
+        {
+            int r = no % 10;
+            ans = ans + p * r;
+            p = p * 2;
+            no = no / 10;
+        }
+        cout << ans << endl;
+        N=N-1;
+    }
+    return 0;
+}
